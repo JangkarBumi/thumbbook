331d9dd57e69d6bb282f3072cb1ef2eeef798f2c
Zeha Irawan <jangkaringbumi@gmail.com>
2019-04-25 21:25:19 +0700

    added post model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save user without email" do
-    user = User.new
-    assert_not user.save, "Saved the article without a title"
+  test 'valid user' do
+    user = User.new(name: 'John', email: 'john@example.com', password: 'foobar')
+    assert user.valid?
+  end
+
+  test "should not save user without email, name or password" do
+   user = User.new
+    assert !user.save
   end
 end
